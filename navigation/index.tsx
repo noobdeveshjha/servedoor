8957024d1008ba3b0ef959179465a27f51528828
noobdeveshjha <noobdeveshjha@gmail.com>
2026-07-06 10:41:43 +0530

    Updated Navigation with Stack + Login flow and Restaurant Detail screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,21 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
 import CartScreen from '../screens/CartScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RestaurantDetail from '../screens/RestaurantDetail';
 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: true }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Search" component={SearchScreen} />
-        <Tab.Screen name="Cart" component={CartScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Main" component={TabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
