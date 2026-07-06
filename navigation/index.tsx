5671d473456ea71e7cdf2b2c735acc5ed6587eab
noobdeveshjha <noobdeveshjha@gmail.com>
2026-07-06 10:38:39 +0530

    Added basic App.tsx and Navigation structure (tabs)

---
+++ navigation/index.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import CartScreen from '../screens/CartScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: true }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Cart" component={CartScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
